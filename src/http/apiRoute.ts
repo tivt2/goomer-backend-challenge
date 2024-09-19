37bf1e1e1a0cd734e1e1e54d859de572e60d3cde
tivt <tivt231@gmail.com>
2024-09-19 14:17:10 -0300

    feat: openapi + cors + swagger-ui-express
    Added openapi documentation with swagger-ui in the /docs endpoint

--- src/http/apiRoute.ts
+++ src/http/apiRoute.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import restaurantsRoute from "./restaurantsRoute";
 
+import swaggerUi from "swagger-ui-express";
+import swaggerJson from "./swagger.json";
+
 const api = Router();
 
 api.get("/ping", (_, res) => {
   res.json({ message: "pong" });
 });
+
+api.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
 api.use("/restaurantes", restaurantsRoute);
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import api from "./http/apiRoute";
+import cors from "cors";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -8,9 +9,10 @@ import { env } from "process";
 const PORT = Number(env.PORT || 3000);
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.use("api", api);
+app.use("/v1", api);
 
 app.listen(PORT, () => {
   console.log(`Server is listening at port: [${PORT}]`);
